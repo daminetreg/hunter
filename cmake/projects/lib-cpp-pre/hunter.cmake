4fdf7b1e8c9a7d1bfe35f0461ef36fb1801f6f16
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2015-11-18 10:39:54 +0100

    CONFIG: Updating lib-cpp-pre to 1.3.2.

--- cmake/projects/lib-cpp-pre/hunter.cmake
+++ cmake/projects/lib-cpp-pre/hunter.cmake
@@ -15,5 +15,12 @@ hunter_add_version(
     SHA1 6733a0abd9f74bd67e4ef521e7677ad9437cdba1 
 )
 
+hunter_add_version(
+    PACKAGE_NAME lib-cpp-pre
+    VERSION "1.3.2"
+    URL "https://github.com/daminetreg/lib-cpp-pre/archive/v1.3.2.tar.gz" 
+    SHA1 8afe8c9d58a3f82dae5a92b2865cf243f3dc2f71
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_download(PACKAGE_NAME lib-cpp-pre)
